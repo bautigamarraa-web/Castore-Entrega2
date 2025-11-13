6eae74ec0301fe7adeaca6d0a9c004b953cadcc7
bautigamarraa-web <bautigamarraa@gmail.com>
2025-11-13 00:37:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-(() => {
-  document.getElementById('year')?.append(new Date().getFullYear());
-  const forms = document.querySelectorAll('.needs-validation');
-  Array.from(forms).forEach(form => {
-    form.addEventListener('submit', event => {
-      if (!form.checkValidity()) {
-        event.preventDefault(); event.stopPropagation();
-      }
-      form.classList.add('was-validated');
-    }, false);
-  });
-})();
+(() => {})();
